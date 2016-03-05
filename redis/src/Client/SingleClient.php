1bfd556df14acd0586b9078b13d8b1d2b2f8bf9d
qii <www.qii@qq.com>
2016-03-05 10:35:48 +0800

    add singleClient support

--- redis/src/Client/SingleClient.php
+++ redis/src/Client/SingleClient.php
@@ -2,11 +2,60 @@
 
 namespace Redis\Client;
 
+use Redis\Proxy;
+
 class SingleClient extends ClientAbstract
 {
-    public function execute()
+    private $link;
+
+    /**
+     * __construct
+     *
+     * @param      array   $config          config of redis, include host, port, weight
+     * @param      string  $redisExtension  type of php redis extension
+     */
+    public function __construct(
+        array $config,
+        $redisExtension = Proxy\RedisFactory::PHPREDIS
+    )
     {
-        //
+        parent::__construct($config, $redisExtension);
+    }
+
+    /**
+     * call the redis object to execute data
+     *
+     * @param      string   $method  method
+     * @param      array   $params  params
+     *
+     * @return     boolean|mixed
+     */
+    public function doExec($method, $params)
+    {
+        // $params[0] is the redis key,and it won't be empty! except randomkey
+        if (empty($params[0]) && ($method != 'randomkey')) {
+            return false;
+        }
+
+        // when randomkey, $params is an empty array,here for avoid php warning
+        empty($params[0]) && $params[0] = '';
+
+        $reids = $this->getConnection();
+
+        return $redis->execute($method, $params);
+    }
+
+    /**
+     * get new redis object, if not cached
+     *
+     * @return     Redis\Proxy\Extensions\ExtensionsAbstract
+     */
+    private function getConnection()
+    {
+        if (!is_null($this->link)) {
+            return $this->link;
+        }
+        return $this->link = $this->createConnection($this->config, $this->redisExtension);;
     }
 
 }
---
+++ redis/src/singleTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use Redis\Proxy;
+use Redis\Client;
+
+
+include 'Autoload.php';
+
+$config = ['host' => '127.0.0.1', 'port' => 6379, 'weight' => 1];
+
+$redis = new Client\SingleClient(
+    $config,
+    Proxy\RedisFactory::PHPREDIS
+);
+
+var_dump($redis);die;
+
+// var_dump($redis->delete('zhangman'));die;
+
+$field = time() . rand(9, 999);
+$value = uniqid('zhangman=');
+
+echo "field is {$field}, value is {$value}\n\n";
+
+var_dump($redis->hset('shibo', $field, $value));
+
+var_dump($redis->hset('shibo1111199', $field, $value));
+
+var_dump($redis->hget('shibo', $field));
+
+var_dump($redis->hget('shibo1111199', $field));
+
+var_dump('=================');
+
+var_dump($redis->set('name', uniqid()));
+var_dump($redis->get('name'));
+var_dump($redis->set('age', uniqid()));
+var_dump($redis->get('age'));
+
+// var_dump($redis);
+// var_dump($redis->randomkey());
