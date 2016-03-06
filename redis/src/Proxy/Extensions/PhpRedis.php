177843144a6b5c45188e386ca0c3381802bac076
qii <www.qii@qq.com>
2016-03-06 10:02:53 +0800

    comment

--- redis/src/Proxy/Extensions/PhpRedis.php
+++ redis/src/Proxy/Extensions/PhpRedis.php
@@ -6,31 +6,30 @@ class PhpRedis extends ExtensionsAbstract
 {
     public function __construct(array $config)
     {
-        //var_dump($config);
         $this->config = $config;
+
         try {
             $this->redis  = new \Redis();
-        } catch (\Exception $e) {
-            die('no phpredis extension...');
         }
-        // var_dump($config['host'], $config['port']);die;
-        try{
+        catch (\Exception $e) {
+            exit($e->geeMessage() . ' Please check your phpredis extension...');
+        }
+
+        try {
             $this->redis->connect($config['host'], $config['port']);
-        }catch (\Exception $e){
-            var_dump($e->getMessage());
+        }
+        catch (\Exception $e) {
+            exit($e->geeMessage() . ' Cant\'t create connection with your phpredis extension...');
         }
     }
 
     public function execute($method, $params)
     {
         var_dump($method . ' from port ' . $this->config['port']);
-        // var_dump($this->redis);
-        // var_dump($method, $params);die;
+
         !is_array($params) && $params = (array) $params;
-        // var_dump($this->config, $method, $params[0], $this->redis->$method($params[0]));die;
+
         return call_user_func_array([$this->redis, $method], $params);
-        // return $re;
-        // var_dump($re);die;
     }
 
     public function __destruct()
